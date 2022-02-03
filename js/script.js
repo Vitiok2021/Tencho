339edd855cac0aef8f6233f2d444ce967cd08480
Vitiok2021 <92989701+Vitiok2021@users.noreply.github.com>
2022-02-03 21:54:45 +0200

    feat: add all files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function mousestart(){
+    let test = document.getElementsByClassName('test')[0];
+    // console.log("11" + test);
+    test.style.left = "0";
+    // console.log(test);
+}
+
+function mousestart2(){
+    let text = document.getElementsByClassName('text')[0];
+    // console.log("11" + test);
+    text.style.left = "0";
+    // console.log(test);
+    let img = document.getElementsByClassName("img")[0];
+    img.style.right = "0";
+}
